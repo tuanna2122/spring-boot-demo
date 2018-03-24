6550ab6ac223a046d2152355a513beecc1b4e779
Tuan Nguyen <tuanna2122@gmail.com>
2018-03-24 11:00:18 +0700

    FeatureA - Added username to User class and name to Role class

--- src/main/java/com/episerver/training/springboot/demos/model/User.java
+++ src/main/java/com/episerver/training/springboot/demos/model/User.java
@@ -4,14 +4,33 @@ public class User {
 
     private Long id;
 
+    private String username;
+
     private String email;
 
     public User(Long id) {
         this.id = id;
     }
 
-    public User(Long id, String email) {
+    public User(Long id, String username, String email) {
         this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 
@@ -21,14 +40,6 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
 }

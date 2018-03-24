f094d26a0fdb564e34474d1ca0d72c25800c0543
Tuan Nguyen <tuanna2122@gmail.com>
2018-03-24 11:08:41 +0700

    FeatureB - Added email to User class and code to Role class

--- src/main/java/com/episerver/training/springboot/demos/model/Role.java
+++ src/main/java/com/episerver/training/springboot/demos/model/Role.java
@@ -4,8 +4,15 @@ public class Role {
 
     private Long id;
 
+    private String code;
+
     public Role(Long id) {
         this.id = id;
+    }
+
+    public Role(Long id, String code) {
+        this.id = id;
+        this.code = code;
     }
 
     public Long getId() {
@@ -15,4 +22,13 @@ public class Role {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
 }

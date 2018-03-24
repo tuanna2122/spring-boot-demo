5a4623a46f4ee2a397cab7a08d2cae3c9f0619eb
Tuan Nguyen <tuanna2122@gmail.com>
2018-03-24 11:00:18 +0700

    FeatureA - Added username to User class and name to Role class

--- src/main/java/com/episerver/training/springboot/demos/model/Role.java
+++ src/main/java/com/episerver/training/springboot/demos/model/Role.java
@@ -4,8 +4,15 @@ public class Role {
 
     private Long id;
 
+    private String name;
+
     public Role(Long id) {
         this.id = id;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public Long getId() {
@@ -14,5 +21,13 @@ public class Role {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/com/episerver/training/springboot/demos/model/User.java
+++ src/main/java/com/episerver/training/springboot/demos/model/User.java
@@ -4,15 +4,30 @@ public class User {
 
     private Long id;
 
+    private String username;
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User(Long id) {
         this.id = id;
     }
 
